97344392dd87f701cef80f016a4ad40989cfd5f9
EgorBo <egorbo@gmail.com>
2016-11-09 19:35:25 +0300

    Initial commit

---
+++ MvpUrhoWorkshop.Droid/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content.PM;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Urho;
+using Urho.Droid;
+
+namespace MvpUrhoWorkshop.Droid
+{
+	[Activity(Label = "MvpUrhoWorkshop.Droid", MainLauncher = true,
+		Icon = "@drawable/icon", Theme = "@android:style/Theme.NoTitleBar.Fullscreen",
+		ConfigurationChanges = ConfigChanges.KeyboardHidden | ConfigChanges.Orientation,
+		ScreenOrientation = ScreenOrientation.Landscape)]
+	public class MainActivity : Activity
+	{
+		protected override async void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			UrhoSurface.RunInActivity<MyGame>(new ApplicationOptions("Data"));
+		}
+
+		protected override void OnResume()
+		{
+			UrhoSurface.OnResume();
+			base.OnResume();
+		}
+
+		protected override void OnPause()
+		{
+			UrhoSurface.OnPause();
+			base.OnPause();
+		}
+
+		public override void OnLowMemory()
+		{
+			UrhoSurface.OnLowMemory();
+			base.OnLowMemory();
+		}
+
+		protected override void OnDestroy()
+		{
+			UrhoSurface.OnDestroy();
+			base.OnDestroy();
+		}
+
+		public override bool DispatchKeyEvent(KeyEvent e)
+		{
+			if (!UrhoSurface.DispatchKeyEvent(e))
+				return false;
+			return base.DispatchKeyEvent(e);
+		}
+
+		public override void OnWindowFocusChanged(bool hasFocus)
+		{
+			UrhoSurface.OnWindowFocusChanged(hasFocus);
+			base.OnWindowFocusChanged(hasFocus);
+		}
+	}
+}
+
---
+++ MvpUrhoWorkshop.HoloLens/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Core;
+using Urho;
+using Urho.Actions;
+using Urho.HoloLens;
+using Urho.Shapes;
+
+namespace MvpUrhoWorkshop.HoloLens
+{
+	/// <summary>
+	/// Windows Holographic application using SharpDX.
+	/// </summary>
+	internal class Program
+	{
+		/// <summary>
+		/// Defines the entry point of the application.
+		/// </summary>
+		[MTAThread]
+		private static void Main()
+		{
+			CoreApplication.Run(new AppViewSource());
+		}
+
+		class AppViewSource : IFrameworkViewSource
+		{
+			public IFrameworkView CreateView() => UrhoAppView.Create<MyGame>(new ApplicationOptions("Data"));
+		}
+	}
+}
